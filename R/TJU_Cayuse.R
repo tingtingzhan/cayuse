8b33b1f7154f5a3cf86374962d8ebcee863d6a24
Tingting Zhan <tingtingzhan@gmail.com>
2025-05-13 10:57:23 -0400

    tzh

---
+++ R/TJU_Cayuse.R
@@ -0,0 +1,190 @@
+
+
+
+#' @title Award & Effort from Cayuse 
+#' 
+#' @description
+#' Print out grant and effort from Cayuse.
+#' 
+#' @param path \link[base]{character} scalar, directory of downloaded award `.csv` file.
+#' Default is the download directory `'~/Downloads'`
+#' 
+#' @param fiscal.year \link[base]{integer} scalar
+#' 
+#' @returns ..
+#' 
+#' @details 
+#' \itemize{
+#' \item {go to `https://jefferson.cayuse424.com/sp/index.cfm` in Chrome (Safari has bugs)}
+#' \item {My Proposals -> Submitted Proposals. 
+#'   Lower-right corner of screen, 'Export to CSV'.
+#'   Downloaded file has name pattern `'^proposals_.*\\.csv'`}
+#' \item {My Awards -> Awards (*not* 'Active Projects').
+#'   Lower-right corner of screen, 'Export to CSV'.
+#'   Downloaded file has name pattern `'^Awards_.*\\.csv'`}
+#' \item {My Awards -> Awards.  Click into each project, under 'People' tab to find my 
+#'   'Sponsored Effort'}
+#' }
+#' 
+#' Function [aggregateAwards] aggregates grant over different period 
+#' (e.g. from Axx-xx-001, Axx-xx-002, Axx-xx-003 to Axx-xx).
+#' Then we need to manually added in our 'Sponsored Effort' in the returned `.csv` file.
+#' 
+#' @examples 
+#' if (FALSE) {
+#' aggregateAwards()
+#' viewProposal()
+#' }
+#' 
+#' @name TJU_Cayuse
+#' @importFrom lubridate year
+#' @importFrom utils read.csv write.table
+#' @importFrom factor.tzh factor<-
+#' @importFrom ThomasJeffersonUniv style_basename
+#' @export
+aggregateAwards <- function(
+    path = '~/Downloads', 
+    fiscal.year = year(Sys.Date())
+) {
+  
+  fls <- list.files(path = path, pattern = '^Awards_.*\\.csv$', full.names = TRUE)
+  if (!length(fls)) stop('Awards file not downloaded?')
+  message('\u261e ', style_basename(fl <- sort.int(fls, decreasing = TRUE)[1L]))
+  
+  awards <- read.csv(file = fl, header = TRUE) |>
+    # subset.data.frame(subset = startsWith(Status, prefix = 'Funded')) # I dont understand this yet
+    subset.data.frame(subset = !nzchar(Flags) & !is.na(Award.End.Date)) |>
+    within.data.frame(expr = {
+      Admin.Unit <- # not relavent
+        Account.Numbers <- # not relavent
+        Status <- # um..
+        Flags <- # used
+        NULL
+      Lead.PI <- gsub(' AOI$', replacement = '', x = Lead.PI)
+      Award.Amount <- gsub('^\\$|,', replacement = '', x = Award.Amount) |> as.double()
+      Award.No. <- Award.No. |>
+        strsplit(split = '-') |> 
+        vapply(FUN = \(i) paste(i[1:2], collapse = '-'), FUN.VALUE = '')
+      Award.Notice.Received <- Award.Notice.Received |> as.Date.character(format = '%m/%d/%Y')
+      Award.Begin.Date <- Award.Begin.Date |> as.Date.character(format = '%m/%d/%Y')
+      Award.End.Date <- Award.End.Date |> as.Date.character(format = '%m/%d/%Y')
+    }) |>
+    subset.data.frame(subset = (Award.Amount > 0)) |>
+    split.data.frame(f = ~ Award.No.) |>
+    lapply(FUN = aggregate_award_) |>
+    do.call(what = rbind.data.frame) |> 
+    within.data.frame(expr = {
+      Status2 <- .bincode(as.double(Award.End.Date), breaks = c(-Inf, as.double(TJU_Fiscal_Year(fiscal.year)), Inf), right = TRUE, include.lowest = TRUE)
+      factor(Status2) <- c(sprintf('Ends before FY%d', fiscal.year), sprintf('Ends in FY%d', fiscal.year), 'Ongoing')
+    }) |>
+    sort_by.data.frame(y = ~ Status2 + Award.End.Date, decreasing = TRUE)
+  
+  view_by_row(awards[c(
+    'Award.No.', 
+    'Status2', 
+    'Project.Title', 'Lead.PI', 'Sponsor', 'Award.Amount', 'Time.Period', 'Award.Period')])
+  
+  # message('\u21ac Fill in `Effort` by clicking into each project under \'Active Projects\'')
+  return(invisible(awards))
+  
+}
+
+
+
+aggregate_award_ <- function(x) {
+  x |> 
+    sort_by.data.frame(y = ~ Award.End.Date, decreasing = TRUE) |>
+    with(expr = {
+      if (!all(duplicated(Project.Title)[-1L])) stop('`Project.Title` not same')
+      first_begin <- min(Award.Begin.Date, na.rm = TRUE)
+      last_end <- max(Award.End.Date, na.rm = TRUE)
+      Award.Period <- paste0(
+        Award.Begin.Date, ' ~ ', Award.End.Date, ', ', 
+        '$', formatC(Award.Amount, big.mark = ',', format = 'f', digits = 2L), collapse = '\n')
+      return(data.frame(
+        Award.No. = Award.No.[1L], 
+        Project.Title = trimws(Project.Title[1L]), 
+        Lead.PI = paste(unique(Lead.PI), collapse = ', '), 
+        Sponsor = Sponsor[1L],
+        Award.Amount = paste0('$', formatC(sum(Award.Amount, na.rm = TRUE), big.mark = ',', format = 'f', digits = 0L)),
+        #Award.Notice.Received = min(Award.Notice.Received, na.rm = TRUE),
+        Time.Period = paste(format.Date(first_begin, format = '%b %d, %Y'), '-', format.Date(last_end, format = '%b %d, %Y')),
+        Award.End.Date = last_end,
+        Award.Period = Award.Period
+      ))
+    })
+}
+
+
+
+
+#' @rdname TJU_Cayuse
+#' @importFrom ThomasJeffersonUniv style_basename
+#' @export
+viewProposal <- function(path = '~/Downloads', fiscal.year = year(Sys.Date())) {
+  
+  proposal_csv_ <- list.files(path = path, pattern = '^proposals_.*\\.csv$', full.names = TRUE)
+  if (!length(proposal_csv_)) stop('Proposal file not downloaded?')
+  message('\u261e ', style_basename(proposal_csv <- sort.int(proposal_csv_, decreasing = TRUE)[1L]))
+  
+  dim(proposal0 <- read.csv(file = proposal_csv, header = TRUE))
+  
+  proposal1 <- within.data.frame(data = proposal0, expr = {
+    Prop.No <- trimws(Prop.No)
+    Lead.PI <- gsub(' AOI$', replacement = '', x = Lead.PI)
+    Submitted.Date <- as.Date.character(Submitted.Date, format = '%m/%d/%Y')
+    Submitted_FY <- .bincode(Submitted.Date, breaks = TJU_Fiscal_Year(fiscal.year)) # NA, 1, NA
+    Submitted_Term <- TJU_SchoolTerm(Submitted.Date)
+  })
+  
+  # status_rm <- c('Not Funded', 'Funded', 'Abandoned', 'Withdrawn', 'TJU Signing Official')
+  status_rm <- c('Funded', 'Abandoned', 'Withdrawn', 'TJU Signing Official')
+  dim(proposal <- eval(quote(subset(x = proposal1, subset = !is.na(Submitted_FY) & !(Status %in% status_rm)))))
+  if (FALSE) { #manually inspect
+    table(proposal$Submitted.Date, useNA = 'ifany')
+    table(proposal$Status, useNA = 'ifany')
+    length(unique(proposal$Lead.PI))
+  }
+  
+  # copy to Interfolio
+  view_by_row(proposal[c(
+    'Status', 'Submitted_Term', 
+    'Project.Name', 'Sponsor', 
+    # 'Prop.No', 
+    'My.Role', 'Lead.PI')])
+  return(invisible(proposal))
+}
+
+
+
+
+
+
+
+if (FALSE) {
+  # only inspect
+  dim(proposalFunded <- subset(pr1, Status == 'Funded')) # to be compared to `awards`
+  # `proposalFunded` is not reliable
+  proposalFunded[c('Lead.PI', 'Project.Name')]
+  subset(awards, OnGoing, select = c('Lead.PI', 'Project.Title'))
+}
+
+
+
+#' @importFrom ThomasJeffersonUniv format_named
+view_by_row <- function(data) {
+  nr <- .row_names_info(data, type = 2L)
+  .mapply(FUN = \(x, nm) {
+    message('Row ', sQuote(nm))
+    x |> format_named() |> lapply(FUN = message)
+    cat('\n')
+  }, dots = list(
+    x = split.data.frame(data, f = seq_len(nr)), 
+    nm = seq_len(nr)
+  ), MoreArgs = NULL)
+  return(invisible())
+}
+
+
+
+
---
+++ R/workday.R
@@ -0,0 +1,206 @@
+
+#' @title Workdays
+#' 
+#' @description
+#' To summarize the number of workdays, weekends, holidays and vacations in a given time-span 
+#' (e.g., a month or a quarter of a year).
+#' 
+#' @param x \link[base]{character} scalar or \link[base]{vector} (e.g.,
+#' `'2021-01'` for January 2021,
+#' `'2021 Q1'` for 2021 Q1 (January to March)), or
+#' \link[base]{integer} scalar or \link[base]{vector} (e.g., `2021L` for year 2021);
+#' The time-span to be summarized.
+#' Objects of classes \link[zoo]{yearqtr} and \link[zoo]{yearmon} are also accepted.
+#' 
+#' @param holiday \link[base]{character} \link[base]{vector}
+#' 
+#' @param vacation \link[base]{Date} \link[base]{vector}, vacation days
+#' 
+#' @details 
+#' 
+#' Function [workday()] summarizes the workdays, weekends,
+#' Jefferson paid holidays 
+#' (New Year’s Day, Martin Luther King, Jr. Day, Memorial Day, Fourth of July, Labor Day, Thanksgiving and Christmas)
+#' and your vacation (e.g., sick, personal, etc.) days (if any),
+#' in a given time-span.
+#' 
+#' Per Jefferson policy (source needed), 
+#' if a holiday is on Saturday, then the preceding Friday is considered to be a weekend day.
+#' If a holiday is on Sunday, then the following Monday is considered to be a weekend day.
+#' 
+#' @returns 
+#' Function [workday()] returns a \link[base]{factor}.
+#' 
+#' @examples
+#' '2025 Q1' |> zoo::as.yearqtr() |> workday() |> table()
+#' 
+#' '2025 Q1' |> 
+#'  zoo::as.yearqtr() |>
+#'  workday(vacation = seq.Date(
+#'   from = as.Date('2025-04-10'), to = as.Date('2025-04-24'), by = 1
+#'  )) |>
+#'  table()
+#'  
+#' 2021L |> workday() |> table()
+#' 
+#' tryCatch(c('2019-10', '2019-12') |> zoo::as.yearmon() |> workday(), error = identity)
+#' c('2019-10' |> zoo::as.yearmon() |> workday(), 
+#'   '2019-12' |> zoo::as.yearmon() |> workday()) |> table() # work-around
+#' 
+#' @keywords internal
+#' @importFrom lubridate year
+#' @importFrom timeDate holiday as.Date.timeDate USNewYearsDay USMLKingsBirthday USMemorialDay USIndependenceDay USLaborDay USThanksgivingDay USChristmasDay
+#' @export
+workday <- function(
+    x, 
+    holiday = c('USNewYearsDay', 'USMLKingsBirthday', 'USMemorialDay', 'USIndependenceDay', 'USLaborDay', 'USThanksgivingDay', 'USChristmasDay'), # https://hr.jefferson.edu/benefits-compensation/paid-time-off.html
+    vacation
+) {
+  
+  if (!length(nx <- length(x_dt <- allDates(x)))) return(invisible()) # use S3
+  if (!all(diff.default(x_dt) == 1L)) stop('Input must be a consecutive time period')
+  
+  # add 1-day before and after, to deal with 'weekend & holiday' situation
+  dt_ext <- c(x_dt[1L] - 1L, x_dt, x_dt[nx] + 1L)
+  
+  wkd <- format.Date(dt_ext, format = '%a') # ?base::weekdays.Date
+  id_holiday <- dt_ext %in% as.Date.timeDate(holiday(year = unique.default(year(dt_ext)), Holiday = holiday))
+  id_weekend <- wkd %in% c('Sat', 'Sun')
+  
+  if (any(id_holiday & id_weekend)) {
+    # holiday on weekend; Jefferson makes the closest weekday as weekend
+    
+    if (length(hSat <- setdiff(which(id_holiday & (wkd == 'Sat')), y = 1L))) {
+      # holiday on Saturday
+      id_weekend[hSat] <- FALSE # Saturday no longer considered as weekend; consider as holiday
+      id_weekend[hSat - 1L] <- TRUE # previous (auxiliary) Friday considered as weekend
+      # dont care when first day is Sunday and before-auxiliary Saturday is holiday or not
+      # Takes care when last day is Friday and after-auxiliary Saturday is a holiday
+    }
+    
+    if (length(wch_Sun <- setdiff(which(id_holiday & (wkd == 'Sun')), y = nx+2L))) {
+      # holiday on Sunday
+      id_weekend[wch_Sun] <- FALSE # Sunday no longer considered as weekend; consider as holiday
+      id_weekend[wch_Sun + 1L] <- TRUE # next (auxiliary) Monday considered as weekend
+      # dont care when last day is Saturday and after-auxiliary Sunday is holiday or not
+      # Takes care when first day is Monday and before-auxiliary Sunday is a holiday
+    }
+    
+  }
+  
+  id_holiday <- id_holiday[-c(1L, nx+2L)]
+  id_weekend <- id_weekend[-c(1L, nx+2L)]
+  if (any(id_holiday & id_weekend)) stop('should have been removed')
+  
+  out <- rep(1L, times = nx) # default: weekday
+  out[id_weekend] <- 2L # weekend
+  out[id_holiday] <- 3L # holiday
+  
+  if (!missing(vacation)) {
+    if (!inherits(vacation, what = 'Date')) stop('`vacation` must be Date object')
+    if (any(vholiday <- vacation %in% x_dt[id_holiday])) {
+      message('Vacation day(s) ', sQuote(vacation[vholiday]), ' are holiday(s).')
+      vacation <- vacation[!vholiday]
+    }
+    if (any(vweekend <- vacation %in% x_dt[id_weekend])) {
+      message('Vacation day(s) ', sQuote(vacation[vweekend]), ' are weekend(s).')
+      vacation <- vacation[!vweekend]
+    }
+    #if (any(vout <- !(vacation %in% x_dt))) {
+    #  message('Vacation day(s) ', sQuote(vacation[vout]), ' are out of the timespan.')
+    #  vacation <- vacation[!vout]
+    #} # this does not change the result :)
+    out[x_dt %in% vacation] <- 4L # vacation
+  } # else do nothing
+  
+  ret <- structure(
+    out,
+    class = 'factor',
+    levels = c('Workday', 'Weekend', 'Holiday', 'Vacation')
+  )
+  factor(ret) # remove zero-count
+}
+
+
+
+
+# Objects \link[zoo]{yearqtr} and \link[zoo]{yearmon} are type-double.
+
+# dont forget
+# base::month.abb
+# base::month.name
+
+#' @title All \link[base]{Date}s in a Time Interval
+#' 
+#' @description
+#' Find all \link[base]{Date}s in a time interval.
+#' 
+#' @param x R objects, such as
+#' \describe{
+#' \item{\link[base]{integer}}{
+#' year, e.g., `x = 2020L` returns all \link[base]{Date}s from 2020-01-01 to 2020-12-31}
+#' \item{\link[zoo]{yearmon}}{
+#' year-month object from package \CRANpkg{zoo}}
+#' \item{\link[zoo]{yearqtr}}{
+#' year-quarter object from package \CRANpkg{zoo}}
+#' \item{\link[base]{character}}{
+#' convertible to \link[zoo]{yearmon} or \link[zoo]{yearqtr} object}
+#' }
+#' 
+#' @details
+#' Function [allDates()] returns all \link[base]{Date}s in a given time interval.
+#' 
+#' @returns 
+#' Function [allDates()] returns a \link[base]{Date} \link[base]{vector}.
+#' 
+#' @keywords internal
+#' @name allDates
+#' @export
+allDates <- function(x) {
+  if (!length(x)) return(invisible())
+  if (inherits(x, what = 'Date')) return(x)
+  if (anyNA(x)) stop('does not allow NA input')
+  x <- unique(x) # ?base::unique.default ?zoo:::unique.yearmon ?zoo:::unique.yearqtr
+  UseMethod(generic = 'allDates')
+}
+
+#' @rdname allDates
+#' @export allDates.integer
+#' @export
+allDates.integer <- function(x) { # `x` considered as year!
+  x |>
+    lapply(FUN = \(i) {
+      i1 <- as.Date.character(paste0(i, c('-01-01', '-12-31')), format = '%Y-%m-%d')
+      seq.Date(from = i1[1L], to = i1[2L], by = 1L)
+    }) |> do.call(what = c)
+}
+
+
+#' @rdname allDates
+#' @importFrom zoo as.Date.yearmon
+#' @export allDates.yearmon
+#' @export
+allDates.yearmon <- function(x) {
+  x |>
+    lapply(FUN = \(i) {
+      seq.Date(from = as.Date.yearmon(i), to = as.Date.yearmon(i + 1/12) - 1L, by = 1L)
+    }) |>
+    do.call(what = c)
+}
+
+#' @rdname allDates
+#' @importFrom zoo as.Date.yearqtr
+#' @export allDates.yearqtr
+#' @export
+allDates.yearqtr <- function(x) {
+  x |>
+    lapply(FUN = \(i) {
+      seq.Date(from = as.Date.yearqtr(i), to = as.Date.yearqtr(i + 1/4) - 1L, by = 1L)
+    }) |> do.call(what = c)
+}
+
+
+
+
+
+
