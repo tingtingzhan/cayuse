788188c65dc7bdf0759355c7337c2bff6f402e75
Tingting Zhan <tingtingzhan@gmail.com>
2025-10-06 23:52:22 -0400

    tzh

--- R/match4.R
+++ R/match4.R
@@ -4,16 +4,24 @@ setGeneric(name = '%in%')
 setOldClass(Classes = 'yearqtr')
 setOldClass(Classes = 'yearmon')
 
-if (FALSE) {
-  match(x = as.Date('2025-10-24'), table = zoo::as.yearqtr('2025 Q4'))
-  as.Date('2025-10-24') %in% zoo::as.yearqtr('2025 Q4')
-  
-  match(x = as.Date('2025-10-24'), table = zoo::as.yearmon('2025-10'))
-  as.Date('2025-10-24') %in% zoo::as.yearmon('2025-10')
-}
-
-
-
+#' @title Match \link[base]{Date} to \link[zoo]{yearmon} and \link[zoo]{yearqtr}
+#' 
+#' @param x \link[base]{Date}
+#' 
+#' @param table \link[zoo]{yearmon} or \link[zoo]{yearqtr}
+#' 
+#' @param nomatch,incomparables see function \link[base]{match}
+#' 
+#' @examples
+#' match(x = as.Date('2025-10-24'), table = zoo::as.yearqtr('2025 Q4'))
+#' stopifnot(as.Date('2025-10-24') %in% zoo::as.yearqtr('2025 Q4'))
+#' 
+#' match(x = as.Date('2025-10-24'), table = zoo::as.yearmon('2025-10'))
+#' stopifnot(as.Date('2025-10-24') %in% zoo::as.yearmon('2025-10'))
+#' @keywords internal
+#' @name match4
+#' @aliases match,Date,yearqtr-method
+#' @export
 setMethod(
   f = match, 
   signature = c(x = 'Date', table = 'yearqtr'), 
@@ -26,7 +34,9 @@ setMethod(
   }
 )
 
-
+#' @rdname match4
+#' @aliases %in%,Date,yearqtr-method
+#' @export
 setMethod(
   f = '%in%',
   signature = c(x = 'Date', table = 'yearqtr'), 
@@ -36,6 +46,9 @@ setMethod(
 ) # must do this, R 4.5.1
 
 
+#' @rdname match4
+#' @aliases match,Date,yearmon-method
+#' @export
 setMethod(
   f = match, 
   signature = c(x = 'Date', table = 'yearmon'), 
@@ -48,6 +61,9 @@ setMethod(
   }
 )
 
+#' @rdname match4
+#' @aliases %in%,Date,yearmon-method
+#' @export
 setMethod(
   f = '%in%',
   signature = c(x = 'Date', table = 'yearmon'), 
